d47856c858bedc5cb4a0b0680dca2461e19c8e53
itsomg134 <omgedam123098@gmail.com>
2025-11-25 21:25:05 +0530

    Add files via upload

---
+++ Teaching ai assistant for biology/frontend/[root-of-the-server]__e1b49361._.js
@@ -0,0 +1,20 @@
+module.exports = [
+"[project]/OneDrive/Desktop/DAILY WELLNESS VOICE COMPANION/frontend/node_modules/.pnpm/camelcase-keys@9.1.3/node_modules/camelcase-keys/index.js [app-route] (ecmascript, async loader)", ((__turbopack_context__) => {
+
+__turbopack_context__.v((parentImport) => {
+    return Promise.all([
+  "server/chunks/52236__pnpm_e15f8bc7._.js"
+].map((chunk) => __turbopack_context__.l(chunk))).then(() => {
+        return parentImport("[project]/OneDrive/Desktop/DAILY WELLNESS VOICE COMPANION/frontend/node_modules/.pnpm/camelcase-keys@9.1.3/node_modules/camelcase-keys/index.js [app-route] (ecmascript)");
+    });
+});
+}),
+"[externals]/node:crypto [external] (node:crypto, cjs, async loader)", ((__turbopack_context__) => {
+
+__turbopack_context__.v((parentImport) => {
+    return Promise.resolve().then(() => {
+        return parentImport("[externals]/node:crypto [external] (node:crypto, cjs)");
+    });
+});
+}),
+];
